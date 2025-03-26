f99282516ae024dc9a7d66aec0d7ccfc13a77741
gitsaulo <147246493+gitsaulo@users.noreply.github.com>
2025-03-26 20:21:31 +0100

    COI 26/03

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,35 @@
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
+import { getContacts } from "./data";
+//usa la funcion getContacts de data.ts, no hace fata poner la extension?
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};//Entiendo que loader es una palabra reservada? que significa el async? y el await?
+//el json() supongo que hace un parse de info a json? porque esta entre {}?
+
+import type { LinksFunction } from "@remix-run/node";
+//esto nos permite usar las funciones de link detro del css y declara el tipo de links
+import appStylesHref from "./app.css?url";
+//la funcion de link dentro del css autoestrae su url
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+]; // Esto es como si hicieramos: <link rel="stylesheet" href="./app.css" />
+//se mete en <Links /> pero... poque esta en mayus la primera? no deberia ser en minuscula?
+
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -34,17 +57,35 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+          {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "/*que es esto?*/}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail">
+            <Outlet />            
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
