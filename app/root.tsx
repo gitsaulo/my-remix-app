6114918b120c466e6cecd035ac0bb36205d585e7
gitsaulo <147246493+gitsaulo@users.noreply.github.com>
2025-03-27 20:08:44 +0100

    COI 2 27/03

--- app/root.tsx
+++ app/root.tsx
@@ -1,27 +1,47 @@
-import { json } from "@remix-run/node";
+import { json,redirect  } from "@remix-run/node";
 import {
   Form,
   Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
-
-import { getContacts } from "./data";
+//La cantida de imports que tiene esto es demencial aprenderme esto es poco viable.
+import { createEmptyContact, getContacts } from "./data";
 //usa la funcion getContacts de data.ts, no hace fata poner la extension?
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
+import { useEffect } from "react";
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
 };//Entiendo que loader es una palabra reservada? que significa el async? y el await?
 //el json() supongo que hace un parse de info a json? porque esta entre {}?
 
-import type { LinksFunction } from "@remix-run/node";
-//esto nos permite usar las funciones de link detro del css y declara el tipo de links
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+//porque este impor es type? esto nos permite usar las funciones de link detro del css y declara el tipo de links
 import appStylesHref from "./app.css?url";
+import { createRequestInit } from "@remix-run/react/dist/data";
 //la funcion de link dentro del css autoestrae su url
+// existing imports
+
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};//esto se supone que detecta un metodo POST?? como sabe cual es el ${contact.id}
+
+
+
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ]; // Esto es como si hicieramos: <link rel="stylesheet" href="./app.css" />
@@ -29,7 +49,23 @@ export const links: LinksFunction = () => [
 
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  //no tengo ni idea de que hace este const
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+  //tampoco tengo ni idea de que hace useEffect
+
   return (
     <html lang="en">
       <head>
@@ -42,10 +78,21 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+          <Form
+              id="search-form"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              role="search"
+            >
               <input
                 id="q"
                 aria-label="Search contacts"
+                className={navigation.state === "loading" && !searching ? "loading" : ""}
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
@@ -61,6 +108,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
+                    <NavLink
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  }
+                  to={`contacts/${contact.id}`}
+                >
                     <Link to={`contacts/${contact.id}`}>
                       {contact.first || contact.last ? (
                         <>
@@ -73,6 +130,7 @@ export default function App() {
                         <span>★</span>
                       ) : null}
                     </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
@@ -83,7 +141,11 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div           
+            className={
+              navigation.state === "loading" ? "loading" : ""
+            } 
+            id="detail">
             <Outlet />            
         </div>
         <ScrollRestoration />
